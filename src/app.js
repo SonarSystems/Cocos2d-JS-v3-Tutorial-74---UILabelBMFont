5d73f0079e80fa5344941002462ebdc581a0332f
Frahaan Hussain <Sonar Systems>
2014-10-20 10:15:33 +0100

    GUI using UILabelBMFont

---
+++ src/app.js
@@ -0,0 +1,37 @@
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var sprite = new cc.Sprite.create(res.CloseNormal_png);
+        sprite.setAnchorPoint(cc.p(0.5, 0.5));
+        sprite.setPosition(cc.p(size.width / 2, size.height / 2));
+        //this.addChild(sprite, 0);
+
+        var labelBMFont = new ccui.TextBMFont();
+        labelBMFont.setFntFile(res.LabelBMFont_png);
+        labelBMFont.setString("BMFont");
+        labelBMFont.x = size.width / 2;
+        labelBMFont.y = size.height / 2;
+        this.addChild(labelBMFont);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
